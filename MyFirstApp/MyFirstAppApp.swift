22cf74b848fd86bec92fe4244149399f7933d94d
donew <eastgloss0330@naver.com>
2024-04-30 20:34:44 +0900

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Dongyun Shin on 4/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
