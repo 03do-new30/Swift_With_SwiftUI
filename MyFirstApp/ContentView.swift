9b19276ed9954aedc4b09fbf15b2f7dbb8181ef2
donew <eastgloss0330@naver.com>
2024-05-04 18:43:06 +0900

    first commit

--- MyFirstApp/ContentView.swift
+++ MyFirstApp/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack{
+            Text("I AM DONEW")
+            Text("I AM DONEW")
+            Text("I AM DONEW")
         }
-        .padding()
+        
     }
 }
 
